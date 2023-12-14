e81a9f97e1dc8bcddb0df85ecab06f96ba0064cd
LouJuly <153835432+LouJuly@users.noreply.github.com>
2023-12-15 00:53:25 +0100

    Add files via upload

--- Galerie.js
+++ Galerie.js
@@ -1,19 +1,19 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     const filterButtons = document.querySelectorAll('.filter-buttons button');
-    const images = document.querySelectorAll('.image');
+    const images = document.querySelectorAll('.category img');
 
     filterButtons.forEach(button => {
         button.addEventListener('click', function () {
             const category = this.getAttribute('data-category');
 
-            // Toggle active class on buttons
+            // Cette partie rend actif le bouton sélectionné
             filterButtons.forEach(btn => btn.classList.remove('active'));
             this.classList.add('active');
 
-            // Show/hide images based on category
+            // cette partie rend visible ou invisible une image selon que le bouton de sa catégorie soit clické
             images.forEach(image => {
-                const imageCategory = image.getAttribute('data-category');
+                const imageCategory = image.parentNode.getAttribute('data-category');
                 if (category === 'all' || category === imageCategory) {
                     image.style.display = 'block';
                 } else {
